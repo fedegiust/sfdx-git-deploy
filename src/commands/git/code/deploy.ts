55871cfd003471db7e41f27867ee5cf76558a7d8
Federico Giust <fgiust@sitetracker.com>
2019-09-03 11:35:35 -0300

    Initial Commit

---
+++ src/commands/git/code/deploy.ts
@@ -0,0 +1,78 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+const shell = require('shelljs');
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('gitDeploy', 'org');
+
+export default class Org extends SfdxCommand {
+
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = [
+    `$ sfdx git:code:deploy --targetusername myOrg@example.com`
+  ];
+
+  public static args = [{name: 'file'}];
+
+  protected static flagsConfig = {
+    // flag with a value (-n, --name=VALUE)
+    branch: flags.string({char: 'b', description: messages.getMessage('branchFlagDescription')}),
+    output: flags.string({char: 'd', description: messages.getMessage('outputDirFlagDescription')})
+  };
+
+  // Comment this out if your command does not require an org username
+  protected static requiresUsername = true;
+
+  // Comment this out if your command does not support a hub org username
+  protected static supportsDevhubUsername = true;
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = false;
+
+  public async run(): Promise<any> {
+
+    let numResults = 0;
+    const results = [];
+
+    if (!shell.which('git')) {
+      shell.echo('Sorry, this script requires git');
+      shell.exit(1);
+    }
+    
+    this.ux.log('Comparing local branch against ' + this.flags.branch);
+    
+    shell.config.verbose = false;
+
+    let workingBranch = shell.exec(`git rev-parse --abbrev-ref HEAD`);
+
+    this.ux.log(`Current working branch is : ${workingBranch}`);
+
+    this.ux.log(`Creating outpud dir: ${this.flags.output}`);
+
+    shell.mkdir('-p', this.flags.output);
+
+    let filesList = shell.exec(`git diff --no-renames --diff-filter=d --name-only ${this.flags.branch} ${workingBranch}`);
+
+    this.ux.log(`List of files to deploy:\n${filesList}`);
+    let filesListArr = filesList.split('\n');
+
+    filesListArr.forEach(element => {
+      let dest = element.substring(0, element.lastIndexOf("/")).replace('force-app', this.flags.output);
+      if (dest != '') {
+        shell.mkdir('-p', dest);
+        results[numResults++] = shell.cp('-r', element, dest);  
+        results[numResults++] = shell.cp('-r', element + '-meta.xml', dest);  
+      }
+    });
+
+    if (!results[numResults - 1].stderr) results[numResults++] = shell.exec(`sfdx force:source:deploy -p "${this.flags.output}" -u ${this.flags.targetusername}`);
+
+    if (!results[numResults - 1].stderr) this.ux.log('Deployed succesfully');
+    // Return an object to be displayed with --json
+    return results[numResults-1];
+  }
+}
